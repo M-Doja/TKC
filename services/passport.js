8e6b591467d49c06e28047a6030ceda3786ece16
M-Doja <micdoja@yahoo.com>
2018-08-04 17:47:56 -0700

    update to db address

--- services/passport.js
+++ services/passport.js
@@ -11,24 +11,24 @@ passport.deserializeUser((id, done) => {
   User.findById(id).then(user => done(null, user));
 });
 
-// passport.use(
-//   new GoogleStrategy(
-//   {
-//     clientID: keys.googleClientID,
-//     clientSecret: keys.googleClientSECRET,
-//     callbackURL: '/auth/google/callback',
-//     proxy: true
-//   },
-//   (accessToken, refreshToken, profile ,done) => {
-//     User.findOne({ googleId: profile.id }).then(existingUser => {
-//       if (existingUser) {
-//         done(null, existingUser);
-//       }else {
-//         new User({
-//           googleId: profile.id,
-//           username: profile.displayName
-//         }).save().then(user => done(null, user));
-//       }
-//     });
-//   })
-// );
+passport.use(
+  new GoogleStrategy(
+  {
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSECRET,
+    callbackURL: '/auth/google/callback',
+    proxy: true
+  },
+  (accessToken, refreshToken, profile ,done) => {
+    User.findOne({ googleId: profile.id }).then(existingUser => {
+      if (existingUser) {
+        done(null, existingUser);
+      }else {
+        new User({
+          googleId: profile.id,
+          username: profile.displayName
+        }).save().then(user => done(null, user));
+      }
+    });
+  })
+);
